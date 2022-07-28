d4dabad228d4a9b0e9194e0ca584dcf82c8b3b8b
Egor Grushin <yrgrushi@mts.ru>
2022-07-28 18:34:26 +0300

    Export undoReducerFactory

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export * from './barrel';
 export * from './blend';
 export * from './distillery';
 export * from './distillerx-connecting.module';
+export { undoReducerFactory } from './reducer-factories';
