36010fe78ff28f20f6fe2e753d269dbe93da112f
Egor Grushin <yrgrushi@mts.ru>
2022-08-04 18:40:19 +0300

    Export types

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,5 @@ export * from './barrel';
 export * from './blend';
 export * from './distillery';
 export * from './distillerx-connecting.module';
+export * from './types';
 export { undoReducerFactory } from './reducer-factories';
